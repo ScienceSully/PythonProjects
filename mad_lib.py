183c33a7c88b0821bbc1fc9766b38373d08db9a8
ScienceSully <93784233+ScienceSully@users.noreply.github.com>
2022-09-05 13:26:55 -0500

    Create mad_lib.py

---
+++ mad_lib.py
@@ -0,0 +1,6 @@
+print("Hello, and welcome to my Madlib!")
+noun1 = input("person ")
+verb1 = input("verb ")
+location1 = input("location ")
+
+print("My hero is ", noun1, ". He is the best ", verb1, " I first met them at ", location1, ".")
