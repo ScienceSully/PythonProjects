442d3ccfbfca3e987578ff3ab516d11b4504b078
ScienceSully <93784233+ScienceSully@users.noreply.github.com>
2022-09-10 20:11:02 -0500

    cleaned up with nested functions

--- NumGuesser.py
+++ NumGuesser.py
@@ -1,25 +1,34 @@
 import random
-import os
-clearing = os.system("cls")
-computer_number = (random.randint(1, 100))
-score = 1
-user_number = input("Guess a number 1-100   ")
-user_number = int(user_number)
 
-while user_number != computer_number:
-  if user_number < computer_number:
-    print("Higher")
-    score += 1
-    user_number = input("Guess again--->")
-    user_number = int(user_number)
-    clearing
+hard = 5
+easy = 10
+print("Welcome to the number guessing game!\nI'm thinking fo a number between 0 and 100")
 
-  if user_number > computer_number:
-    print("Lower")
-    score += 1
-    user_number = input("Guess again--->")
-    user_number = int(user_number)
-    clearing
+def select_level():
+  diff = " "
+  while diff != "easy" and diff != "hard":
+    diff = input("Choose a difficulty. Type: 'easy' or 'hard': ")
+  if diff == "easy":
+    return(easy)
+  if diff == "hard":
+    return(hard)
 
-else:
-    print("Congradulations!!!" + " It took you ", score, " guesses.")
+def game():
+  answer = random.randint(0,100)
+  numb_guesses = select_level()
+  user_guess = int(input("Make a guess: "))
+  while numb_guesses != 1:
+    if user_guess > answer:
+      print("Too high.")
+    elif user_guess < answer:
+      print("Too low.")
+    else:
+      print("Thats correct! You win.")
+      return
+    numb_guesses -= 1
+    print("You have",numb_guesses,"guesses remaining to guess the number.")
+    user_guess = int(input("guess a number: "))
+  if numb_guesses == 1:
+    print(f"You have no remaining guesses. answer:{answer}")
+
+game()
